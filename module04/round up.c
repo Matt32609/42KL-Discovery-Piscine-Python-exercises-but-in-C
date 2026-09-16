007ee876166dbc8553156ca70333ce6e8801def7
Matt32609 <vthematt@gmail.com>
2026-09-16 23:03:26 +0800

    Add files via upload

---
+++ module04/round up.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+    {float number;
+    printf("Please enter a number");scanf("%f", &number);
+    printf("%.0f\n", ceilf(number));
+
+    return 0;}
