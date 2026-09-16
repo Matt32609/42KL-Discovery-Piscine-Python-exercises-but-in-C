ad737db1e6610a153b3a632e3b63eaabf21b05ce
Matt32609 <vthematt@gmail.com>
2026-09-16 23:01:45 +0800

    Add files via upload

---
+++ module04/age.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+    {int age;
+    printf("Please type your age:");scanf("%d", &age);
+
+    printf("You are now %d years old.\n", age);
+    printf("In 10 years , you will be %d years old.\n", age + 10);
+    printf("In 20 years , you will be %d years old.\n", age + 20);
+    printf("In 30 years , you will be %d years old.\n", age + 30);
+
+    return 0;}
+
