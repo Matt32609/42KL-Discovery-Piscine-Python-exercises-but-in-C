6895723926fcb82f513297ff3e22bcdd30554e5b
Matt32609 <vthematt@gmail.com>
2026-09-16 23:06:21 +0800

    Add files via upload

---
+++ module06/aft first param.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+    {if (argc > 2)
+        printf("%s\n", argv[1]);
+    else
+        printf("None");
+    
+    return 0;}
